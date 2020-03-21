0f1d397235985d93e44bf46c833d1945dae8ae4b
Thomas Haug <thomas.haug@outlook.com>
2020-03-21 16:40:37 +0100

    Hotfix Services

---
+++ src/sites/samples.js
@@ -0,0 +1,13 @@
+import React, { useState, setState, useEffect } from 'react';
+
+//import StoresService from './../services/stores';
+//import ProductsService from './../services/products';
+
+export default function Samples() {
+    return (
+        <div>
+            <h2>Samples</h2>
+            <h3>Stores</h3>
+        </div>
+    );
+}
