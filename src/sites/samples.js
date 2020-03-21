34ba716c2c72e3f4d674ea573ae205825010884d
Thomas Haug <thomas.haug@outlook.com>
2020-03-21 16:44:40 +0100

    Restore Services with fix

--- src/sites/samples.js
+++ src/sites/samples.js
@@ -1,13 +1,20 @@
 import React, { useState, setState, useEffect } from 'react';
 
-//import StoresService from './../services/stores';
+import StoresService from './../services/stores';
 //import ProductsService from './../services/products';
 
 export default function Samples() {
+    const [data, setData] = useState({});
+    useEffect(() => {
+        const StoreServiceInstance = new StoresService();
+        const SampleDataStores = StoreServiceInstance.getStores('1').then(setData)
+    }, []);
+
     return (
         <div>
             <h2>Samples</h2>
             <h3>Stores</h3>
+            {JSON.stringify(data)}
         </div>
     );
 }
