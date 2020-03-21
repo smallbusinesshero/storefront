479bf656cd97ff44dce41f4b69bb14bdacf22fc5
Jannis Ötjengerdes <joetjengerdes@rightmart.de>
2020-03-21 22:56:14 +0100

    Add store profile page

--- src/App.js
+++ src/App.js
@@ -1,98 +1,92 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import HomeIcon from '@material-ui/icons/Home';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import HomeIcon from "@material-ui/icons/Home";
+import Container from "@material-ui/core/Container";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import About from './sites/aboutus';
-import Home from './sites/home';
-import Samples from './sites/samples';
+import About from "./sites/aboutus";
+import Home from "./sites/home";
+import Samples from "./sites/samples";
 import StoreProfile from "./sites/StoreProfile";
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
+    display: "flex"
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
+      duration: theme.transitions.duration.leavingScreen
+    })
   },
   appBarShift: {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+      duration: theme.transitions.duration.enteringScreen
+    })
   },
   menuButton: {
-    marginRight: 36,
+    marginRight: 36
   },
   hide: {
-    display: 'none',
+    display: "none"
   },
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
-    whiteSpace: 'nowrap',
+    whiteSpace: "nowrap"
   },
   drawerOpen: {
     width: drawerWidth,
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+      duration: theme.transitions.duration.enteringScreen
+    })
   },
   drawerClose: {
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
+      duration: theme.transitions.duration.leavingScreen
     }),
-    overflowX: 'hidden',
+    overflowX: "hidden",
     width: theme.spacing(7) + 1,
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9) + 1,
-    },
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1
+    }
   },
   toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
+    ...theme.mixins.toolbar
   },
   content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
+    flexGrow: 1
+  }
 }));
 
 export default function App() {
@@ -115,7 +109,7 @@ export default function App() {
         <AppBar
           position="fixed"
           className={clsx(classes.appBar, {
-            [classes.appBarShift]: open,
+            [classes.appBarShift]: open
           })}
         >
           <Toolbar>
@@ -125,76 +119,80 @@ export default function App() {
               onClick={handleDrawerOpen}
               edge="start"
               className={clsx(classes.menuButton, {
-                [classes.hide]: open,
+                [classes.hide]: open
               })}
             >
               <MenuIcon />
             </IconButton>
             <Typography variant="h6" noWrap>
               Small Business Hero
-          </Typography>
+            </Typography>
           </Toolbar>
         </AppBar>
         <Drawer
-          variant="permanent"
           className={clsx(classes.drawer, {
             [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
+            [classes.drawerClose]: !open
           })}
           classes={{
             paper: clsx({
               [classes.drawerOpen]: open,
-              [classes.drawerClose]: !open,
-            }),
+              [classes.drawerClose]: !open
+            })
           }}
         >
           <div className={classes.toolbar}>
             <IconButton onClick={handleDrawerClose}>
-              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+              {theme.direction === "rtl" ? (
+                <ChevronRightIcon />
+              ) : (
+                <ChevronLeftIcon />
+              )}
             </IconButton>
           </div>
           <Divider />
           <List>
             <Link to="/">
               <ListItem button>
-                <ListItemIcon><HomeIcon /></ListItemIcon>
+                <ListItemIcon>
+                  <HomeIcon />
+                </ListItemIcon>
                 <ListItemText>Home</ListItemText>
               </ListItem>
             </Link>
             <Link to="/about">
               <ListItem button>
-                <ListItemIcon><InboxIcon /></ListItemIcon>
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
                 <ListItemText>About</ListItemText>
               </ListItem>
             </Link>
             <Link to="/samples">
               <ListItem button>
-                <ListItemIcon><InboxIcon /></ListItemIcon>
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
                 <ListItemText>Samples</ListItemText>
               </ListItem>
             </Link>
           </List>
         </Drawer>
         <main className={classes.content}>
-          <div className={classes.toolbar} />
-          <Container>
-
-            {/* A <Switch> looks through its children <Route>s and
+          {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-            <Switch>
-              <Route path="/stores/:storeId" component={StoreProfile} />
-              <Route path="/samples">
-                <Samples />
-              </Route>
-              <Route path="/about">
-                <About />
-              </Route>
-              <Route path="/">
-                <Home />
-              </Route>
-            </Switch>
-          </Container>
-
+          <Switch>
+            <Route path="/stores/:storeId" component={StoreProfile} />
+            <Route path="/samples">
+              <Samples />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
         </main>
       </div>
     </Router>
--- src/sites/StoreProfile.js
+++ src/sites/StoreProfile.js
@@ -1,36 +1,187 @@
-import React, {useEffect, useState} from 'react';
-import {useParams} from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import PropTypes from "prop-types";
 import StoresService from "../services/stores";
+import "./StoreProfile.css";
+import Fab from "@material-ui/core/Fab";
+import PhoneEnabledIcon from "@material-ui/icons/PhoneEnabled";
+
+const staticData = {
+  id: "d290f1ee-6c54-4b01-90e6-d701748f0851",
+  name: { de_DE: "Golden Neukölln" },
+  subTitle: { de_DE: "Künstler-Zubehör" },
+  phone: "0421-123123123",
+  description: {
+    de_DE: `Bei golden! findest du alles, was dich glücklich macht.
+    
+    „Symbiose“ bringt es ganz gut auf den Punkt, wenn es um die Beschreibung des Ladens vom Mutter-Tochter-Duo Margret
+    und Gisa Schleef geht. Alte und neue Produkte sind gleichwertig vereint. Nostalgie und Moderne können sich auf
+    40qm Fläche entfalten. Dazwischen sprießen frische Blumen und Zimmerpflanzen. Die Inhaberinnen beweisen Geschmack
+    bei der Gestaltung als auch bei der Produktauswahl in ihrem Laden.\n\ngolden! heißt das gemeinsame Projekt der
+    beiden und ist in der Sonnenallee in Neukölln zu finden. Komm vorbei, lass dich inspirieren, gönn dir eine
+    Kleinigkeit und mach es dir anschließend Zuhause gemütlich. Denn hier wird auf Qualität und Nachhaltigkeit
+    geachtet. Da die alten gesammelten Schätzchen Einzelstücke sind, wechselt das Angebot stetig. Auch das
+    Blumen- und Pflanzenangebot ist je nach Saison wechselnd. In golden! steckt Liebe und Herzblut zweier kreativer
+    Unternehmer-Frauen.`
+  },
+  address: {
+    streetName: "Sonnenallee",
+    streetNumber: "64",
+    postalCode: "12045",
+    city: "Berlin",
+    region: null,
+    state: null,
+    country: "DE"
+  },
+  neighborhood: ["Berlin West"],
+  geoLocation: null,
+  goods: [
+    {
+      name: "Kleine Tasche",
+      price: 3500,
+      image:
+        "https://images.unsplash.com/photo-1523779105320-d1cd346ff52b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1652&q=80"
+    },
+    {
+      name: "Große Tasche",
+      price: 1400,
+      image:
+        "https://images.unsplash.com/photo-1523779105320-d1cd346ff52b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1652&q=80"
+    },
+    {
+      name: "Hamster",
+      price: 1200,
+      image:
+        "https://images.unsplash.com/photo-1523779105320-d1cd346ff52b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1652&q=80"
+    }
+  ],
+  image:
+    "https://images.unsplash.com/photo-1517971071642-34a2d3ecc9cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
+};
 
 const StoreProfile = props => {
+  const [storeData, setStoreData] = useState(null);
+  const { storeId } = useParams();
 
-    const [storeData, setStoreData] = useState(null);
-    const {storeId} = useParams();
+  const [shorten, setShorten] = useState(true);
 
-    useEffect(()=>{
-        fetchStoreData();
-    },[]);
+  const shortWordCount = 30;
 
-    const fetchStoreData = () => {
-        const StoreServiceInstance = new StoresService();
-        StoreServiceInstance.getStore(storeId).then(setStoreData);
-    };
+  useEffect(() => {
+    fetchStoreData();
+  }, []);
 
-    return (
-        <React.Fragment>
-            {storeData &&
-            <div id="#store-profile">
-                <h1>{storeData.name.de_DE}</h1>
-                <p>{storeData.description.de_DE}</p>
+  const fetchStoreData = () => {
+    setStoreData(staticData);
+    //const StoreServiceInstance = new StoresService();
+    //StoreServiceInstance.getStore(storeId).then(setStoreData);
+  };
+
+  const getShortenedText = () => {
+    return storeData.description.de_DE
+      .split(" ")
+      .slice(0, shortWordCount)
+      .join(" ");
+  };
+
+  const isShortened = () => {
+    return storeData.description.de_DE.split(" ").length > shortWordCount;
+  };
+
+  return (
+    <>
+      {storeData ? (
+        <>
+          <div className="header">
+            <div
+              className="sp-header-background"
+              style={{ backgroundImage: `url(${storeData.image})` }}
+            ></div>
+            <a href={`tel:${storeData.phone}`}>
+              <Fab
+                color="primary"
+                aria-label="phone_enabled"
+                className="sp__telephone-button"
+              >
+                <PhoneEnabledIcon />
+              </Fab>
+            </a>
+          </div>
+          <div className="sp__content">
+            <div id="store-profile">
+              <h1 className="sp__title">{storeData.name.de_DE}</h1>
+              <h3 className="sp__subtitle">{storeData.subTitle.de_DE}</h3>
+
+              <p className="sp__description">
+                {shorten ? getShortenedText() : storeData.description.de_DE}
+                {isShortened() &&
+                  (shorten ? (
+                    <>
+                      <span>… </span>
+                      <a
+                        href=""
+                        onClick={e => {
+                          setShorten(false);
+                          e.preventDefault();
+                        }}
+                      >
+                        weiterlesen
+                      </a>
+                    </>
+                  ) : (
+                    <>
+                      {" "}
+                      <a
+                        href=""
+                        onClick={e => {
+                          setShorten(true);
+                          e.preventDefault();
+                        }}
+                      >
+                        weniger
+                      </a>
+                    </>
+                  ))}
+              </p>
             </div>
-            }
-        </React.Fragment>
-    );
+
+            <div className="sp_goods">
+              {storeData.goods.map(good => (
+                <div className="sp_goods-good">
+                  <div className="sp_goods-good-image-wrapper">
+                    <div
+                      className="sp_goods-good-image"
+                      style={{ backgroundImage: `url(${good.image})` }}
+                    ></div>
+                  </div>
+                  <div className="sp_goods-good-info">
+                    <div className="sp_goods-good-name">{good.name}</div>
+                    <div className="sp_goods-good-price">
+                      {good.price / 100} €
+                    </div>
+                  </div>
+                  <div className="sp_goods-good-iconbar">
+                    <span className="sp_goods-good-shopping-card">
+                      <span className="material-icons">shopping_cart</span>
+                    </span>
+                    <span className="sp_goods-good-bookmark">
+                      <span className="material-icons">bookmark</span>
+                    </span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <span>Lade...</span>
+        </>
+      )}
+    </>
+  );
 };
 
-StoreProfile.propTypes = {
-
-};
+StoreProfile.propTypes = {};
 
 export default StoreProfile;
