324a621ed970716c77f4b99314129d1e9c86ebe4
Felix Leber <felix.leber@diconium.com>
2020-04-04 20:03:20 +0200

    Reformat with prettier, no functional changes

--- services/analytics.tsx
+++ services/analytics.tsx
@@ -1,33 +1,33 @@
 import ReactGA from "react-ga";
 
 export function initializeAnalytics() {
-  // Only on client side 
-  if (typeof window !== 'undefined') {
+  // Only on client side
+  if (typeof window !== "undefined") {
     // On local, setup testMode, so data won't go to analytics dashboard
     if (process.env.NODE_ENV === "development") {
-      ReactGA.initialize('UA-162524264-1', { testMode: true, debug: false });
+      ReactGA.initialize("UA-162524264-1", { testMode: true, debug: false });
     } else {
-      ReactGA.initialize('UA-162524264-1');
+      ReactGA.initialize("UA-162524264-1");
     }
 
-  ReactGA.pageview(window.location.pathname + window.location.search);
-    }
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
 }
 
-// Send event if user uses search bar and log place id 
+// Send event if user uses search bar and log place id
 export function analyticsSearchLocation(placeID: string) {
   ReactGA.event({
-      category: 'user-input',
-      action: 'search-location',
-      label: placeID,
-    });
+    category: "user-input",
+    action: "search-location",
+    label: placeID
+  });
 }
 
-	// Send event event if social-media link is clicked
+// Send event event if social-media link is clicked
 export function analyticsSocialMediaRef(eventType: string, storeId: string) {
   ReactGA.event({
-      category: 'social-media-ref',
-      action: eventType,
-      label: storeId,
-    });
+    category: "social-media-ref",
+    action: eventType,
+    label: storeId
+  });
 }
