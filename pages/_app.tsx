841ded9cf7829afe1e4f4a8e68ed91fff4d6774f
Thomas Haug <thomas.haug@outlook.com>
2020-04-12 00:15:26 +0200

    Refactoring to new design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,138 +1,178 @@
 // import App from 'next/app'
-import React from 'react';
-import Head from 'next/head';
-import {makeStyles, ThemeProvider} from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import {useEffect} from 'react';
-import theme from '../theme/Mui';
-import './_app.css';
+import React from "react";
+import Head from "next/head";
+import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { useEffect } from "react";
+import theme from "../theme/Mui";
+import "./_app.css";
 
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Link from '../components/atoms/Link';
-import BackgroundImg from './../assets/images/main-bg.jpg';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Container from "@material-ui/core/Container";
+import Link from "../components/atoms/Link";
 
-const useStyles = makeStyles(theme => ({
-	root: {
-		minHeight: '100vh',
-		background: `white url(${BackgroundImg}) no-repeat bottom`,
-		backgroundSize: 'cover',
-		display: 'flex',
-		flexGrow: 1
-	},
-	header: {
-		display: 'flex',
-		justifyContent: 'space-between',
-		alignItems: 'center'
-	},
-	appBar: {
-		backgroundColor: 'transparent',
-		fontWeight: 'bold',
-		zIndex: theme.zIndex.drawer + 1,
-		boxShadow: 'none',
-	},
-	logo: {
-		textDecoration: 'none'
-	},
-	logoSubtitle: {
-		color: '#000',
-		fontWeight: 'normal'
-	},
-	logoSubtitleHighlight: {
-		color: '#00944a',
-		fontWeight: 'bold'
-	},
-	toolbar: {
-		backgroundColor: 'rgba(255, 255, 255, 1)',
-		color: '#00944a',
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'space-between',
-		padding: theme.spacing(0, 1),
-		// necessary for content to be below app bar
-		...theme.mixins.toolbar
-	},
-	content: {
-		flexGrow: 1,
-	},
-	appname: {
-		color: 'black',
-		fontSize: '1.1em',
-		margin: '0'
-	},
-	container: {
-		marginTop: '3em'
-	}
+import {
+  CustomButton,
+  CustomButtonHighlight,
+} from "../components/atoms/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: "100vh",
+    display: "flex",
+    flexGrow: 1,
+  },
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  appBar: {
+    backgroundColor: "transparent",
+    zIndex: theme.zIndex.drawer + 1,
+    boxShadow: "none",
+  },
+  logo: {
+    textDecoration: "none",
+  },
+  logoSubtitle: {
+    color: "#2a2a2a",
+    fontWeight: "bold",
+  },
+  logoSubtitleHighlight: {
+    color: "#2a2a2a",
+    fontWeight: "lighter",
+  },
+  register: {
+    height: "64px",
+  },
+  toolbar: {
+    backgroundColor: "rgba(255, 255, 255, 1)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+  },
+  appname: {
+    color: "black",
+    fontSize: "1.1em",
+    margin: "0",
+  },
+  container: {
+    marginTop: "3em",
+  },
+  wevsvirus: {
+    lineHeight: "64px",
+    paddingLeft: "10px",
+    fontWeight: "bold",
+  },
+  wevsvirusHeart: {
+    color: "red",
+  },
 }));
 
-const SmallBusinessHeroApp = ({Component, pageProps}) => {
+const SmallBusinessHeroApp = ({ Component, pageProps }) => {
+  const classes = useStyles();
 
-	const classes = useStyles();
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  });
 
-	useEffect(() => {
-		// Remove the server-side injected CSS.
-		const jssStyles = document.querySelector('#jss-server-side');
-		if (jssStyles) {
-			jssStyles.parentElement.removeChild(jssStyles);
-		}
-	});
+  return (
+    <React.Fragment>
+      <Head>
+        <meta charSet="utf-8" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon_16x16.ico"
+          sizes="16x16"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon_32x32.ico"
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon_48x48.ico"
+          sizes="48x48"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
 
-	return <React.Fragment>
-		<Head>
-			<meta charSet='utf-8'/>
-			<link rel='icon' type='image/png' href='/favicon_16x16.ico' sizes='16x16'/>
-			<link rel='icon' type='image/png' href='/favicon_32x32.ico' sizes='32x32'/>
-			<link rel='icon' type='image/png' href='/favicon_48x48.ico' sizes='48x48'/>
-			<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'/>
-			<link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons'/>
-			<meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width'/>
-		</Head>
-		<ThemeProvider theme={theme}>
-			{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-
-			<div className={classes.root}>
-				<AppBar
-					color={'transparent'}
-					position='fixed'
-					className={classes.appBar}
-				>
-					<Toolbar className={classes.toolbar}>
-						<Link href='/' className={classes.logo}>
-							<div style={{display: 'flex', alignItems: 'center'}}>
-								<img
-									src='/assets/images/logo.png'
-									style={{
-										width: '64px',
-										height: 'auto'
-									}}
-								/>
-								<Typography variant='h6' className={classes.logoSubtitle}>
-									small business{' '}
-									<span className={classes.logoSubtitleHighlight}>
-										hero
-									</span>
-								</Typography>
-							</div>
-							{/*  Placeholder text for logo*/}
-						</Link>
-						<Link href="https://docs.google.com/forms/d/1qVNaUAvrFfpXZvRB3ZKv-1RelD6GTnbejSulJMfqUwg/viewform?edit_requested=true" target="_blank" rel="noopener">Registriere deinen Laden</Link>
-						 
-					</Toolbar>
-				</AppBar>
-				<CssBaseline/>
-				<main className={classes.content}>
-					<div className={classes.toolbar}/>
-					<Container className={classes.container}>
-						<Component {...pageProps} />
-					</Container>
-				</main>
-			</div>
-
-		</ThemeProvider>
-	</React.Fragment>
-}
+        <div className={classes.root}>
+          <AppBar
+            color={"transparent"}
+            position="fixed"
+            className={classes.appBar}
+          >
+            <Toolbar className={classes.toolbar} disableGutters={true}>
+              <Link
+                href="https://docs.google.com/forms/d/1qVNaUAvrFfpXZvRB3ZKv-1RelD6GTnbejSulJMfqUwg/viewform?edit_requested=true"
+                target="_blank"
+                rel="noopener"
+                className={classes.wevsvirus}
+              >
+                #WeVsVirus <span className={classes.wevsvirusHeart}>❤</span>
+              </Link>
+              <Link href="/" className={classes.logo}>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <span className={classes.logoSubtitle}>
+                    SBH.{" "}
+                    <span className={classes.logoSubtitleHighlight}>
+                      small business hero
+                    </span>
+                  </span>
+                </div>
+                {/*  Placeholder text for logo*/}
+              </Link>
+              <CustomButtonHighlight
+                href="https://docs.google.com/forms/d/1qVNaUAvrFfpXZvRB3ZKv-1RelD6GTnbejSulJMfqUwg/viewform?edit_requested=true"
+                target="_blank"
+                rel="noopener"
+                className={classes.register}
+              >
+                Registrieren
+              </CustomButtonHighlight>
+            </Toolbar>
+          </AppBar>
+          <CssBaseline />
+          <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Container className={classes.container}>
+              <Component {...pageProps} />
+            </Container>
+          </main>
+        </div>
+      </ThemeProvider>
+    </React.Fragment>
+  );
+};
 
 // Only uncomment this method if you have blocking data requirements for
 // every single page in your application. This disables the ability to
@@ -146,4 +186,4 @@ const SmallBusinessHeroApp = ({Component, pageProps}) => {
 //   return { ...appProps }
 // }
 
-export default SmallBusinessHeroApp
+export default SmallBusinessHeroApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,96 +1,110 @@
-import React, { useState } from 'react';
-import { Container, SvgIcon } from '@material-ui/core';
-import StoresService from '../services/stores';
-import CardCarousel from '../components/molecules/CardCarousel';
-import useStyles from '../assets/styles/index_styles';
-import mockData from '../assets/mock-data/mock-data';
-import AddressIcon from '../assets/icons/ecommerce/Address.svg';
-import {Button, Typography} from '@material-ui/core';
+import React, { useState } from "react";
+import { Container, SvgIcon } from "@material-ui/core";
+import StoresService from "../services/stores";
+import CardCarousel from "../components/molecules/CardCarousel";
+import useStyles from "../assets/styles/index_styles";
+import mockData from "../assets/mock-data/mock-data";
+import AddressIcon from "../assets/icons/ecommerce/Address.svg";
+import { Typography } from "@material-ui/core";
+
+import { CustomButton } from "../components/atoms/Button";
 
 export default function Home() {
-	const classes = useStyles();
-	const [kiez, setKiez] = useState('');
-	const [filteredLocations, setFilteredLocations] = useState([]);
+  const classes = useStyles();
+  const [kiez, setKiez] = useState("");
+  const [filteredLocations, setFilteredLocations] = useState([]);
 
-	const [storeData, setStoreData] = useState([]);
+  const [storeData, setStoreData] = useState([]);
 
-	const fetchStoreData = async location => {
-		const StoreServiceInstance = new StoresService();
-		setStoreData(await StoreServiceInstance.getStores(location));
-	};
+  const fetchStoreData = async (location) => {
+    const StoreServiceInstance = new StoresService();
+    setStoreData(await StoreServiceInstance.getStores(location));
+  };
 
-	return (
-		<Container maxWidth={'md'} className={classes.wrapper}>
-			<div className={classes.searchPanel}>
-				<Typography className={classes.intro}>
-					<span className={classes.introHeadline}>Sei ein Held zu Zeiten von Corona.</span> <br />Rette den Laden bei Dir um die
-					Ecke. Hier kannst Du helfen.
-				</Typography>
-				<form
-					className={classes.form}
-					onSubmit={e => {
-						e.preventDefault();
-						fetchStoreData(kiez);
-					}}
-				>
-					<div className={classes.searchWrapper}>
-						<label htmlFor='home__search' className={classes.searchLabel}>
-							Adresse oder Stadtviertel
-						</label>
-						<SvgIcon className={classes.searchMarker} viewBox={"0 0 20 50"}><AddressIcon /></SvgIcon>
-						<input
-							id='home__search'
-							className={classes.search}
-							type='text'
-							autoComplete='off'
-							value={kiez}
-							placeholder='z.B. Donaustraße, Berlin'
-							onChange={e => {
-								setKiez(e.target.value);
-								setFilteredLocations(
-									e.target.value
-										? mockData.kiezList.filter(location =>
-											location
-												.toLowerCase()
-												.match(e.target.value.toLowerCase())
-										)
-										: []
-								);
-							}}
-						/>
-						{kiez && filteredLocations && filteredLocations.length > 0 && (
-							<ul className={classes.selectionList}>
-								{filteredLocations.map(option => (
-									<li
-										key={option}
-										className={classes.selectionListItem}
-										onClick={e => {
-											e.preventDefault();
-											setKiez(option);
-											setFilteredLocations([]);
-											fetchStoreData(option);
-										}}
-									>
-										{option}
-									</li>
-								))}
-							</ul>
-						)}
-					</div>
-					<br />
-					<Button fullWidth={true} disableFocusRipple={true} disableRipple={true} onClick={e => {
-						//e.preventDefault();
-						fetchStoreData(kiez);
-					}}>Suchen </Button>
-				</form>
-			</div>
-			{storeData.length > 0 ? (
-				<>
-					<CardCarousel storeData={storeData} />
-				</>
-			) : (
-				kiez.length > 0 && `Keine Ergebnisse für Suche nach '${kiez}'`
-			)}
-		</Container>
-	);
+  return (
+    <Container maxWidth={"md"} className={classes.wrapper}>
+      <div className={classes.searchPanel}>
+        <Typography className={classes.intro}>
+          <span className={classes.introHeadline}>
+            Sei ein Held zu Zeiten von Corona.
+          </span>{" "}
+          <br />
+          Rette den Laden bei Dir um die Ecke. Hier kannst Du helfen.
+        </Typography>
+        <form
+          className={classes.form}
+          onSubmit={(e) => {
+            e.preventDefault();
+            fetchStoreData(kiez);
+          }}
+        >
+          <div className={classes.searchWrapper}>
+            <label htmlFor="home__search" className={classes.searchLabel}>
+              Adresse oder Stadtviertel
+            </label>
+            <SvgIcon className={classes.searchMarker} viewBox={"0 0 20 50"}>
+              <AddressIcon />
+            </SvgIcon>
+            <input
+              id="home__search"
+              className={classes.search}
+              type="text"
+              autoComplete="off"
+              value={kiez}
+              placeholder="z.B. Donaustraße, Berlin"
+              onChange={(e) => {
+                setKiez(e.target.value);
+                setFilteredLocations(
+                  e.target.value
+                    ? mockData.kiezList.filter((location) =>
+                        location
+                          .toLowerCase()
+                          .match(e.target.value.toLowerCase())
+                      )
+                    : []
+                );
+              }}
+            />
+            {kiez && filteredLocations && filteredLocations.length > 0 && (
+              <ul className={classes.selectionList}>
+                {filteredLocations.map((option) => (
+                  <li
+                    key={option}
+                    className={classes.selectionListItem}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setKiez(option);
+                      setFilteredLocations([]);
+                      fetchStoreData(option);
+                    }}
+                  >
+                    {option}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+          <br />
+          <CustomButton
+            fullWidth={true}
+            disableFocusRipple={true}
+            disableRipple={true}
+            onClick={(e) => {
+              //e.preventDefault();
+              fetchStoreData(kiez);
+            }}
+          >
+            Suchen{" "}
+          </CustomButton>
+        </form>
+      </div>
+      {storeData.length > 0 ? (
+        <>
+          <CardCarousel storeData={storeData} />
+        </>
+      ) : (
+        kiez.length > 0 && `Keine Ergebnisse für Suche nach '${kiez}'`
+      )}
+    </Container>
+  );
 }
